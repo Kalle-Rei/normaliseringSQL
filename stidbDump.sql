788728536248d7aa9d9335448813fadfdf56f750
Kalle-Rei <kalle_reithmeier@hotmail.com>
2021-11-25 10:30:31 +0100

    complete normalisation of Phones

--- stidbDump.sql
+++ stidbDump.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `Hobby`
 --
 USE stidb;
+SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `Hobby`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
