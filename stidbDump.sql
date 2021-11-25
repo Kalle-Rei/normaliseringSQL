3d95ac08ea6a75b6c6f6b8a0a60f4e6e56d27cf8
Kalle-Rei <kalle_reithmeier@hotmail.com>
2021-11-25 10:07:27 +0100

    use the correct database

--- stidbDump.sql
+++ stidbDump.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `Hobby`
 --
-
+USE stidb;
 DROP TABLE IF EXISTS `Hobby`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
